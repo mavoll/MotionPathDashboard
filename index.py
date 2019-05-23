7cd9cdead314e3a7fdd03a2fbf9fd28239da2234
Marc <mavoll@users.noreply.github.com>
2019-05-23 21:59:25 +0200

    Update index.py

--- index.py
+++ index.py
@@ -36,4 +36,6 @@ def display_page(pathname):
         return '404'
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,
+                   threaded=True,
+                   port=8050)
